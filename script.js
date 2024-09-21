c817bd9d891153e3d4ac8dcf4cc1a888504b1486
adceballos <78570037+adceballos@users.noreply.github.com.]>
2024-09-20 20:40:15 -0500

    Create hamburger menu button that appears on smaller screens to organize nav links and used javascript to open and close nav drawer on click

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementById('menu-btn');
+const navLinks = document.getElementById('nav-links');
+
+menuButton.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
