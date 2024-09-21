95abc70f902de87aa99719299a5047a757ff163c
adceballos <78570037+adceballos@users.noreply.github.com.]>
2024-09-21 01:30:54 -0500

    Add light/dark mode functionality, initialize about page, and make minor styling adjustments to header elements

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const menuButton = document.getElementById('menu-btn');
 const navLinks = document.getElementById('nav-links');
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
 
 menuButton.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+})
+
